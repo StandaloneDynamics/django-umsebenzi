ca2849a8122d0375bde2f7d8f20b472c2811453c
ebsuku <midnight.complex@protonmail.com>
2024-01-05 14:08:34 +0200

    remove trailing slash

--- umsebenzi/urls.py
+++ umsebenzi/urls.py
@@ -2,6 +2,6 @@ from rest_framework import routers
 
 from umsebenzi.views import ProjectViewSet, TaskViewSet
 
-router = routers.DefaultRouter()
+router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'projects', ProjectViewSet, basename='project')
 router.register(r'tasks', TaskViewSet, basename='task')

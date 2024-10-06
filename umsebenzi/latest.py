ad511d1a95b293a9df1bd8ccf96a16adb4660a03
knightebsuku <midnight.complex@protonmail.com>
2024-10-06 13:25:30 +0200

    check if 1st task

--- umsebenzi/latest.py
+++ umsebenzi/latest.py
@@ -4,9 +4,11 @@ from umsebenzi.exceptions import TaskCodeException
 
 def get_task_code(project : Project) -> int:
     """Get latest projects task code and increment by 1"""
-    task = Task.objects.filter(project=project).latest('id')
     try:
+        task = Task.objects.filter(project=project).latest('id')
         code = int(task.code.split('-')[-1])
         return code + 1
     except (IndexError, ValueError):
         raise TaskCodeException
+    except Task.DoesNotExist:
+        return 1
